5cd3be114e0ae1cd9ac233a653e5e02106318612
Tanay Mukherjee <its_tanay_here@yahoo.com>
2020-03-16 05:07:41 -0400

    Update elasticsearch.py

--- nyc_parking_violations/part2/src/bigdata1/elasticsearch.py
+++ nyc_parking_violations/part2/src/bigdata1/elasticsearch.py
@@ -18,15 +18,3 @@ def create_and_update_index(index_name, doc_type):
 
     return es
 
-def data_formatting(ref):
-    for key, value in ref.items():
-        if 'amount' in key:
-            ref[key] = float(value)
-        elif 'number' in key:
-            item[key] = int(value)            
-        elif 'date' in key:
-            ref[key] = datetime.strptime(ref[key], '%m/%d/%Y').date()
-
-def call_ref(ref, es, index):
-    data_formatting(ref)
-    res = es.index(index=index, body=ref, id=ref['summons_number'])
